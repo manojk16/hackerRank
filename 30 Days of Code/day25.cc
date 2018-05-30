bb6b41ecbda8bc4c18298b30bb49844245d3f0f8
manoj <manoj.kumar@kaiostech.com>
2018-05-31 00:56:07 +0530

    Adding New Solution Method

--- 30 Days of Code/day25.cc
+++ 30 Days of Code/day25.cc
@@ -30,7 +30,7 @@ Sample Output
 Not prime
 Prime
 Prime
-*/
+
 
 vector<long> sieveOfEratosthenes() {
   vector<bool> primes;
@@ -93,4 +93,56 @@ int main() {
         cout << "Not prime" << endl;
     }
   }
+}
+*/
+
+
+// ******************************Method II*********************************************
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool checkPrime( int in_numb )
+{
+    if( in_numb == 0 || in_numb == 1 )
+    {
+        return false;
+    }
+    
+    //  If a number is divisable by another number less or equal to the square root of the first number... it is NOT prime.
+    int sq = sqrt( in_numb );
+    for (int i = 2; i <= sq; i++)
+    {
+        //if divisible
+        if (in_numb % i == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int T;
+    
+    cin >> T;
+        
+    for( int i = 0; i < T; i++ )
+    {
+        int numb;
+        cin >> numb;
+                
+        bool b =checkPrime( numb );
+        if( b )
+            cout << "Prime" << endl;
+        else
+            cout << "Not prime" << endl;
+    }  
+    
+    return 0;
 }
