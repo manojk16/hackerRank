a5360019c3b6a80a34617da622f44e66f4ca83f4
manoj <manoj.kumar@kaiostech.com>
2018-06-04 00:39:36 +0530

    New Program covers all Test Cases

--- 30 Days of Code/day26.cc
+++ 30 Days of Code/day26.cc
@@ -48,7 +48,7 @@ Sample Output
 45
 */
 
-int main() {
+/*int main() {
     int returnDay, returnMonth, returnYear;
     int dueDay, dueMonth, dueYear;
     int fine;
@@ -67,4 +67,33 @@ int main() {
     }
 
     cout << fine << endl;
+}
+*/
+int main(){
+
+int eday, emon, eyear;
+int aday, amon, ayear;
+
+cin >> aday >> amon >> ayear;
+cin >> eday >> emon >> eyear;
+
+int fine = 0;
+
+int dyear = ayear - eyear;
+int dmon = amon - emon;
+int dday = aday - eday;
+
+if (dyear > 0) {
+    fine = 10000;
+}
+else if (dmon > 0 && dyear == 0) {
+    fine = 500 * dmon;
+}
+else if (dday > 0 && dmon == 0) {
+    fine = 15 * dday;
+}
+
+cout << fine << endl;
+
+    return 0;
 }
